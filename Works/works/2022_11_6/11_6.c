a98a33317f6df0f38d5bfe5f6196e8b3ce1b0de5
Yukii2333 <yds.sx.rqy2333@gmail.com>
2022-11-06 23:05:41 +0800

    2022_11_6.c

---
+++ Works/works/2022_11_6/11_6.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int a[] = { 0 };
+	//init(a);
+	scanf("%d". a);
+	printf("%d\n", a);
+	return 0;
+}
+
+//void exchange(char a[], char b[])
+//{
+//	int left = 0;
+//	int right = strlen(a);
+//	char tmp = { 0 };
+//
+//	while (left <= right)
+//	{
+//		tmp = a[left];
+//		a[left] = b[left];
+//		b[left] = tmp;
+//		left++;
+//	}
+//
+//}
+//
+//int main()
+//{
+//	char a[] = "bit!!";
+//	char b[] = "hello";
+//	printf("³õÊ¼:a[]=%s  b[]=%s\n", a, b);
+//	exchange(a, b);
+//	printf("½»»»:a[]=%s  b[]=%s\n", a, b);
+//	return 0;
+//}
