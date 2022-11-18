c0d964408c890e01d19c864747074e535a065b30
Yukii2333 <yds.sx.rqy2333@gmail.com>
2022-11-18 19:41:55 +0800

    2022_11_18/11_18.c

---
+++ Works/works/2022_11_18/11_18.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void P(int n)
+{
+	int one = 0;
+	int ten = 0;
+	int hun = 0;
+	int thu = 0;
+	for (int i = 1; i < 1000; i++)
+	{
+
+	}
+
+}
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	P(n);
+	return 0;
+}
+
+//void ways(int n)
+//{
+//	int ca = 0;
+//	int pi = 0;
+//	int ve = 0;
+//	for (ca = 1; ca < 34; ca++)
+//	{
+//		for (pi = 1; pi < 50; pi++)
+//		{
+//			for (ve = 1; ve < 100; ve++)
+//			{
+//				if (ca * 3 + pi * 2 + ve * 1 == n)
+//				{
+//					printf("cattle=%d,pig=%d,vegetable=%d\n", ca, pi, ve);
+//				}
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	ways(n);
+//	return 0;
+//}
