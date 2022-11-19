ccd704d28399e6ef159f6029a5cb67f8900f3077
Yukii2333 <yds.sx.rqy2333@gmail.com>
2022-11-19 18:46:36 +0800

    2022_11_19/11_19.c

--- Works/works/2022_11_19/11_19.c
+++ Works/works/2022_11_19/11_19.c
@@ -2,27 +2,52 @@
 
 #include <stdio.h>
 
-void main()
-{
-	int n = 0;
-	int i = 0; 
-	int e[100] = { 0 };
-	int c = 0;;
-	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", &e[i]);
-	}
+//int main()
+//{
+//	int m = 0, n = 0, i = 0, j = 0, count = 0;
+//	scanf("%d %d", &m, &n);
+//	for (i = m; i < n; i++)
+//	{
+//		int tmp = 1;
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				tmp = 0;
+//				break;
+//			}
+//		}
+//		if (tmp)
+//		{
+//			printf("%d\n", i);
+//			count++;
+//		}
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
 
-	c = e[0];
-	for (i = 0; i < n; i++)
-	{
-
-		if (e[i] > c)
-		{
-			c = e[i];
-		}
-	}
-
-	printf("%d\n", c);
-}
+//void main()
+//{
+//	int n = 0;
+//	int i = 0; 
+//	int e[100] = { 0 };
+//	int c = 0;;
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &e[i]);
+//	}
+//
+//	c = e[0];
+//	for (i = 0; i < n; i++)
+//	{
+//
+//		if (e[i] > c)
+//		{
+//			c = e[i];
+//		}
+//	}
+//
+//	printf("%d\n", c);
+//}
