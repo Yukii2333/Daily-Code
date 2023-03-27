9f470a137fc7c29e998641943493ed16311cc44f
Yukii2333 <yds.sx.rqy2333@gmail.com>
2023-03-27 13:04:06 +0800

    2023_03_23/03_23.c

--- Works/works/2023_03_25/03_25.c
+++ Works/works/2023_03_25/03_25.c
@@ -105,3 +105,64 @@ void myCircularQueueFree(MyCircularQueue* obj) {
 }
 
 */
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// 从字符串中删除空格和换行符
+void remove_space_and_newline(char* str) {
+    int i, j;
+    for (i = 0, j = 0; str[i] != '\0'; i++) {
+        if (!isspace((unsigned char)str[i]) && str[i] != '\r' && str[i] != '\n') {
+            str[j] = str[i];
+            j++;
+        }
+    }
+    str[j] = '\0';
+}
+
+int main() {
+    printf("输入学生数量：");
+    int num = 0;
+    scanf("%d", &num);
+    int i, j;
+    // 循环处理每个学生的作业
+    for (i = 1; i <= num; i++) {
+        for (j = i + 1; j <= num; j++) {
+            // 创建并打开学生作业文件
+            char filename1[20], filename2[20];
+            snprintf(filename1, sizeof(filename1), "Student%d.txt", i);   // 获取第一个学生作业的文件名
+            snprintf(filename2, sizeof(filename2), "Student%d.txt", j);   // 获取第二个学生作业的文件名
+            FILE* fp1 = fopen(filename1, "r");   // 打开第一个学生作业文件
+            FILE* fp2 = fopen(filename2, "r");   // 打开第二个学生作业文件
+
+            // 如果无法打开文件，退出程序
+            if (fp1 == NULL || fp2 == NULL) {
+                printf("不能打开文件\n");
+                exit(1);
+            }
+
+            // 从文件中读取字符串
+            char content1[1000], content2[1000];
+            fgets(content1, sizeof(content1), fp1);   // 从第一个学生作业文件中读取字符串
+            fgets(content2, sizeof(content2), fp2);   // 从第二个学生作业文件中读取字符串
+
+            // 关闭文件
+            fclose(fp1);
+            fclose(fp2);
+
+            // 从字符串中删除空格和换行符
+            remove_space_and_newline(content1);
+            remove_space_and_newline(content2);
+
+            // 如果两个学生的作业重复，请打印消息并标记出来
+            if (strcmp(content1, content2) == 0) {   // 如果两个学生作业内容相同
+                printf("学生%d的作业和学生%d的作业重复\n", i, j);   // 输出重复消息
+            }
+        }
+    }
+    return 0;
+}
