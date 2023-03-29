d1693b8365310485957a822e0e68a6aed4a23b4a
Yukii2333 <yds.sx.rqy2333@Gmail.com>
2023-03-29 22:54:43 +0800

    2023_03_29

--- Works/works/2023_03_28/03_28.c
+++ Works/works/2023_03_28/03_28.c
@@ -5,3 +5,30 @@ int main()
 	printf("Hello World!\n");
 	return 0;
 }
+
+/*
+int k = 0;
+while (k < n - gap)
+{
+	for (int i = k; i < n - gap; i += gap)
+	{
+		int gap = 3;
+		int end = i;
+		int tmp = a[end + gap];
+		while (end >= 0)
+		{
+			if (tmp < a[end])
+			{
+				a[end + gap] = a[end];
+				end -= gap;
+			}
+			else
+			{
+				break;
+			}
+		}
+		a[end + gap] = tmp;
+	}
+	k += gap;
+}
+*/
