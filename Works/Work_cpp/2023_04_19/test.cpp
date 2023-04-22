95a703afb14e9b8e91f2e07594ca0c8164e0f77c
Yukii2333 <yds.sx.rqy2333@Gmail.com>
2023-04-22 20:57:25 +0800

    new

---
+++ Works/Work_cpp/2023_04_19/test.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define ADD(x,y) ((x)+(y))
+
+
+int main()
+{
+
+	return 0;
+}
---
+++ Works/Work_cpp/2023_04_22/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+inline int Add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int ret = Add(1, 2);
+	return 0;
+}
